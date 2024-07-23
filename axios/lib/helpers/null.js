7dbafc985a3db775d0986b84c5b1873e8ce612cd
深海鱼 <254108795@qq.com>
2024-07-23 18:25:38 +0800

    rollp-config

--- axios/lib/helpers/null.js
+++ axios/lib/helpers/null.js
@@ -1,2 +1,13 @@
 // eslint-disable-next-line strict
+// 该文件与package.json中的browser字段相关
+// 在rollup打包时，通过@rollup/plugin-node-resolve中的browser字段
+// 将模块的解析将重定向到当前模块
+// 这样就可以避免在浏览器中加载特定于nodejs的代码
+/**
+  "browser": {
+    "./lib/adapters/http.js": "./lib/helpers/null.js",
+    "./lib/platform/node/index.js": "./lib/platform/browser/index.js",
+    "./lib/platform/node/classes/FormData.js": "./lib/helpers/null.js"
+  },
+ */
 export default null;
--- axios/rollup.config.js
+++ axios/rollup.config.js
@@ -1,64 +1,77 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
-import json from '@rollup/plugin-json';
-import { babel } from '@rollup/plugin-babel';
-import autoExternal from 'rollup-plugin-auto-external';
-import bundleSize from 'rollup-plugin-bundle-size';
-import aliasPlugin from '@rollup/plugin-alias';
+import resolve from '@rollup/plugin-node-resolve'; // 用于解析node_modules中的模块
+import commonjs from '@rollup/plugin-commonjs'; // 用于将CommonJS模块转换为ES6模块
+import { terser } from "rollup-plugin-terser"; // 用于压缩代码
+import json from '@rollup/plugin-json'; // 允许rollup处理json文件
+import { babel } from '@rollup/plugin-babel'; // 用于将ES6+的代码转换为ES5
+import autoExternal from 'rollup-plugin-auto-external'; // 用于自动将dependencies和peerDependencies标记为外部模块
+import bundleSize from 'rollup-plugin-bundle-size'; // 显示打包后的文件大小
+import aliasPlugin from '@rollup/plugin-alias'; // 创建路径别名
 import path from 'path';
 
+
 const lib = require("./package.json");
+
+// 定义打包后的文件名、库的名称以及不同的入口文件
 const outputFileName = 'axios';
 const name = "axios";
 const namedInput = './index.js'; // 命名导出的入口文件
 const defaultInput = './lib/axios.js'; // 默认导出的入口文件
 
+/**
+ *  构建配置函数
+ */
 const buildConfig = ({ es5, browser = true, minifiedVersion = true, alias, ...config }) => {
   const { file } = config.output;
-  const ext = path.extname(file);
-  const basename = path.basename(file, ext);
+  const ext = path.extname(file); // 提取文件的扩展名（含点）
+  const basename = path.basename(file, ext); // 提取文件名
+  // 提取文件的扩展名（不包含点）
   const extArr = ext.split('.');
   extArr.shift();
 
 
   const build = ({ minified }) => ({
-    input: namedInput,
-    ...config,
+    input: namedInput, // 入口文件
+    ...config, // 基本配置项
     output: {
-      ...config.output,
+      ...config.output, // 传入的output
+      // 重新定义文件名，根据是否需要压缩来决定最终的文件名
       file: `${path.dirname(file)}/${basename}.${(minified ? ['min', ...extArr] : extArr).join('.')}`
     },
     plugins: [
-      aliasPlugin({
+      aliasPlugin({ // 创建路径别名
         entries: alias || []
       }),
-      json(),
-      resolve({ browser }),
+      json(), // 允许rollup处理json文件
+      resolve({ browser }), // 使用package.json中的browser字段解析
       commonjs(),
 
-      minified && terser(),
-      minified && bundleSize(),
-      ...(es5 ? [babel({
+      minified && terser(), // 压缩代码
+      minified && bundleSize(), // 显示打包后的文件大小
+      ...(es5 ? [babel({ // ES5转换
         babelHelpers: 'bundled',
         presets: ['@babel/preset-env']
       })] : []),
-      ...(config.plugins || []),
+      ...(config.plugins || []), // 传入的插件列表
     ]
   });
 
+  // 生成配置
   const configs = [
     build({ minified: false }),
   ];
 
+  // 添加压缩版本
   if (minifiedVersion) {
     configs.push(build({ minified: true }))
   }
 
+  // 返回最后的配置
   return configs;
 };
 
+// 导出主配置
 export default async () => {
+  // 版权信息
   const year = new Date().getFullYear();
   const banner = `// Axios v${lib.version} Copyright (c) ${year} ${lib.author} and contributors`;
 
